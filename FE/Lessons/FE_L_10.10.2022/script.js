8385b42e70b2095a90486d9b89fbced7e157357d
Tolik4umak <Tolik4umak@gmail.com>
2022-10-10 23:51:18 +0200

    hw git

---
+++ FE/Lessons/FE_L_10.10.2022/script.js
@@ -0,0 +1,74 @@
+
+
+// let n1 = 10;
+// let n2 = 5;
+
+// let res = n1++ + n2;
+// console.log(res);
+
+// res = n1 + n2;
+// console.log(res);
+
+// console.log(n1);
+
+
+
+
+
+
+
+
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// for (let i = 0; i<0; i++){
+//     console.log(i);
+// }
+
+// let j=0;
+// do {
+//     console.log(j);
+//     j++
+// }while(j<0);
+
+
+
+// let i = 3;
+// while (i) {
+//     console.log(i--)
+// }
+
+
+// let j = 10;
+
+// while(j-->=5){
+//     console.log(j)
+// }
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+    
+// }
+
+
+
+function showGreeting () {
+    let messege = "hello";
+    console.log(messege);
+
+    return "TOLIK"
+};
+
+
+showGreeting();
+
+console.log(showGreeting());
