c67d62889d9b4898c8e03c90481575e7a2aba062
Tolik4umak <Tolik4umak@gmail.com>
2022-12-11 21:43:34 +0100

    TRANSLATE_CARDS_6

--- FE/Consultations/TRAINING/Translate_cards/script.js
+++ FE/Consultations/TRAINING/Translate_cards/script.js
@@ -5,6 +5,7 @@ const originalWord = document.querySelector('.original_word')
 const translationWord = document.querySelector('.translated_word')
 const addButton = document.querySelector('.addToList')
 const rotateButton = document.querySelector('.rotateAll')
+const searchInput = document.querySelector('.search')
 
 if(!localStorage.getItem('currentSide')){
     localStorage.setItem('currentSide','frontside')
@@ -13,7 +14,7 @@ if(!localStorage.getItem('currentSide')){
 const cards = document.querySelector('.cards')
 
 
-rerender()
+rerender(wordList)
 
 rotateButton.addEventListener('click',rotateAllCards)
 
@@ -31,58 +32,63 @@ form.addEventListener('submit', (event)=>{
     originalWord.value = '';
     translationWord.value = '';
     
-    rerender()
-    console.log(wordList)
+    rerender(wordList)
 })
 
+searchInput.addEventListener('input',(event)=> findFilter(event))
 
-function rerender(){
+
+
+
+function rerender(arr){
     cards.innerText = ''
-    wordList.forEach((card)=>{
-
-        const container = document.createElement('div')
-        const del = document.createElement('div')
-        const box = document.createElement('div')
-        const front = document.createElement('div')
-        const back = document.createElement('div')
-
-        container.classList.add('card')
-        del.classList.add('del')
-        box.classList.add('card_container')
-        front.classList.add('card_front')
-        front.classList.add('side')
-        back.classList.add('card_back')
-        back.classList.add('side')
-
+    arr.forEach((card, _ ,myArr)=>{
+        const container = createCard(card, _ ,myArr) 
         cards.append(container)
-        container.append(del,box)
-        del.innerText ='X'
-        box.append(front,back)
-        front.innerText = card.original
-        back.innerText = card.translate
-
-        if(localStorage.getItem('currentSide') == 'backside'){
-            box.classList.add('rotate180')
-        }
-
-        del.addEventListener('click',(event)=> delCard(card.id))
-        box.addEventListener('click',rotateCard)
-
     })   
 }
 
+function createCard(card, _ ,myArr){
+    const container = document.createElement('div')
+    const del = document.createElement('div')
+    const box = document.createElement('div')
+    const front = document.createElement('div')
+    const back = document.createElement('div')
 
-function delCard(cardId){
+    container.classList.add('card')
+    del.classList.add('del')
+    box.classList.add('card_container')
+    front.classList.add('card_front')
+    front.classList.add('side')
+    back.classList.add('card_back')
+    back.classList.add('side')
+
+    container.append(del,box)
+    del.innerText ='X'
+    box.append(front,back)
+    front.innerText = card.original
+    back.innerText = card.translate
+
+    if(localStorage.getItem('currentSide') == 'backside'){
+        box.classList.add('rotate180')
+    }
+    del.addEventListener('click',(event)=> delCard(myArr,card.id))
+    box.addEventListener('click',rotateCard)
+
+    return container
+}
+
+function delCard(arr, cardId){
     wordList = wordList.filter(el => el.id != cardId);
     localStorage.setItem('translation',JSON.stringify(wordList))
-    rerender()
+
+    arr = arr.filter(el => el.id != cardId);
+    rerender(arr)
 }
 
 function rotateCard(){
     this.classList.toggle('rotate180')
 }
-
-
 function rotateAllCards(event){
     const rotateList = document.querySelectorAll('.card_container');
 
@@ -111,3 +117,11 @@ function rotateAllCards(event){
     }
 }
 
+function findFilter(event){
+    let currentValue = event.target.value.toLowerCase();
+    let TMParr = wordList.filter(({original,translate})=>{
+        return original.toLowerCase().includes(currentValue)||translate.toLowerCase().includes(currentValue)
+    })
+    console.log(TMParr)
+    rerender(TMParr)
+}
